db50a79fc82d1e868891a8a94c73f3d7ff009440
tanakaworld <yutaro.tanaka.world@gmail.com>
2017-06-07 12:00:22 +0900

    Rename directory

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,13 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
-    template: './client/index.html',
+    template: './src/index.html',
     filename: 'index.html',
     inject: 'body'
 });
 
 module.exports = {
-    entry: './client/index.js',
+    entry: './src/index.js',
     output: {
         path: path.resolve('dist'),
         // below line only works for webpack 1.0
